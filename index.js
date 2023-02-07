bfa3c74b74f60fc3efbc751127f06a4db06449f6
rmkofficial <kmert0@outlook.com>
2023-02-07 16:50:28 +0300

    class, text method

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
+
+$("h1").text("Bye");
+$("h1").addClass("big-title margin-50");
+$("button").html("<em>Don't click me</em>");
+
+// $("h1").removeClass("big-title");
