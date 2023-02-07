21578671df3b521bec2f5ee74160267c48be976d
rmkofficial <kmert0@outlook.com>
2023-02-07 19:42:21 +0300

    attr

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 $("h1").text("Bye");
 $("h1").addClass("big-title margin-50");
 $("button").html("<em>Don't click me</em>");
+$("a").attr("href", "https://www.yahoo.com");
 
 // $("h1").removeClass("big-title");
