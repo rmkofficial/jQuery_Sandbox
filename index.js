7ec713b2d18b4ab1fb524c2b93528cc3ce146f37
rmkofficial <kmert0@outlook.com>
2023-02-07 20:55:48 +0300

    callback function

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 // $("h1").css("color", "red");
-
-$("h1").text("Bye");
-$("h1").addClass("big-title margin-50");
-$("button").html("<em>Don't click me</em>");
-$("a").attr("href", "https://www.yahoo.com");
-
+// $("h1").text("Bye");
+// $("h1").addClass("big-title margin-50");
 // $("h1").removeClass("big-title");
+// $("button").html("<em>Don't click me</em>");
+// $("a").attr("href", "https://www.yahoo.com");
+$("h1").addClass("big-title margin-50");
+$("h1").html("<em>Hello</em>");
+
+$("h1").click(myFunction());
+
+function myFunction() {
+  $("h1").css("color", "yellow");
+}
+
+
+$("button.pu").click(function () {
+  $("h1").css("color", "purple");
+});
+$("button.b").click(function () {
+  $("h1").css("color", "blue");
+});
+$("button.g").click(function () {
+  $("h1").css("color", "green");
+});
+$("button.pi").click(function () {
+  $("h1").css("color", "pink");
+});
+
+
+$("body").keypress(function(event){
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color","aqua");
+})
+
